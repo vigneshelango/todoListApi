b593b9aea8eea45acb0e0ab680f761d7aa77f436
vigneshelango <33080645+vigneshelango@users.noreply.github.com>
2017-11-11 13:13:25 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,40 @@
+var express = require('express'),
+app = express(),
+port = process.env.PORT || 3000,
+mongoose = require('mongoose'),
+Task = require('./api/models/todoListModel'), //created model loading here
+bodyParser = require('body-parser');
+
+
+// mongoose instance connection url connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/Tododb'); 
+console.log(mongoose.connection.readyState);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+
+  
+console.log('=========================');
+console.log(req.body)
+console.log('=========================')
+
+  next();
+});
+
+
+
+var routes = require('./api/routes/todoListRoutes'); //importing route
+routes(app); //register the route
+
+
+
+app.listen(port);
+
+
+console.log('todo list RESTful API server started on: ' + port);
+
